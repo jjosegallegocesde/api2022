c8ee0bb388eb887d5ad6db29797833175f23ad65
jjosegallegomesa <juan.gallegomesa@gmail.com>
2022-04-13 08:39:00 -0500

    se crean servicios

--- routes/rutas.js
+++ routes/rutas.js
@@ -1,8 +1,10 @@
 import express from 'express'
-
-
-import {buscarJugador} from '../controllers/controlador.js'
+import {Controlador} from '../controllers/controlador.js'
 
 export let rutas=express.Router()
+let controlador=new Controlador()
 
-rutas.get('/API/v1/jugadores', buscarJugador)
+rutas.get('/API/v1/general', controlador.buscar)
+rutas.get('/API/v1/general/:id', controlador.buscarPorId)
+rutas.post('/API/v1/general', controlador.insertar)
+rutas.put('/API/v1/general/:id', controlador.editar)
--- server/ServidorModelo.js
+++ server/ServidorModelo.js
@@ -1,28 +1,33 @@
 //const express = require('express')
 import express from 'express'
 import {rutas} from '../routes/rutas.js'
+import {establecerConexion} from '../database/conexion.js'
 
 export class Servidor{
 
     constructor(){
-
         this.app = express()
+        this.conectarConBD()
+        this.auxiliares()
         this.enrutarPeticiones()
-
     }
 
     despertarServidor(){
-
         this.app.listen(process.env.PORT,function(){
             console.log(process.env.PORT)
         })
     }
 
+    conectarConBD(){
+        establecerConexion()
+    }
+
     enrutarPeticiones(){
-
        this.app.use("/",rutas)
+    }
 
-
+    auxiliares(){
+        this.app.use(express.json())
     }
 
 
